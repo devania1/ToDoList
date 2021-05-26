8cd9d6535d04e88b0f2470b2ff6843f79a158b1f
Anna Sobanska <ania.webdev@gmail.com>
2021-05-26 11:32:05 +0200

    add fall animation to trash

--- app.js
+++ app.js
@@ -41,7 +41,11 @@ function deleteCheck(e){
     //delete todo
     if(item.classList[0] === 'trash-btn'){
         const todo = item.parentElement;
-        todo.remove();
+        //Animation
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend', function(){
+            todo.remove();    
+        });
     }
     //check mark
     if(item.classList[0] === "complete-btn"){
